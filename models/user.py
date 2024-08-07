a362f5410321c78ab3c7d6f7b341fe60b5da49d0
Fellipe Marcellus <marcellusfellipe92@gmail.com>
2024-08-06 22:58:34 -0300

    Updating user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,7 @@
 from app import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(8), nullable=False, unique=False)
